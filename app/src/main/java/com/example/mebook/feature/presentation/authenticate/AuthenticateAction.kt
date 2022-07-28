654a03bc9b4d252f82d2636e717de53058bbec7f
AmirHossein <amirhosseinghafoorian>
2022-07-28 13:32:47 +0430

    - update AuthenticateAction.kt to sealed interface

--- app/src/main/java/com/example/mebook/feature/presentation/authenticate/AuthenticateAction.kt
+++ app/src/main/java/com/example/mebook/feature/presentation/authenticate/AuthenticateAction.kt
@@ -1,8 +1,8 @@
 package com.example.mebook.feature.presentation.authenticate
 
-sealed class AuthenticateAction {
-    data class SnackBar(val message: String) : AuthenticateAction()
-    data class ChangeName(val name: String) : AuthenticateAction()
-    data class Navigate(val route: String) : AuthenticateAction()
-    object NavigateUp : AuthenticateAction()
+sealed interface AuthenticateAction {
+    data class SnackBar(val message: String) : AuthenticateAction
+    data class ChangeName(val name: String) : AuthenticateAction
+    data class Navigate(val route: String) : AuthenticateAction
+    object NavigateUp : AuthenticateAction
 }
