cc2e4692a637edb4f89941f2206862d9cde0b94b
AmirHossein <amirhosseinghafoorian>
2022-08-16 20:45:57 +0430

    - setup data module

--- app/src/main/java/com/example/mebook/di/RepositoryModule.kt
+++ app/src/main/java/com/example/mebook/di/RepositoryModule.kt
@@ -1,6 +1,12 @@
 package com.example.mebook.di
 
+import com.example.mebook.data.repository.DataStoreRepositoryImpl
+import com.example.mebook.data.repository.LocalRepositoryImpl
+import com.example.mebook.data.repository.RemoteRepositoryImpl
 import com.example.mebook.data.repository.TestRepositoryImpl
+import com.example.mebook.domain.DataStoreRepository
+import com.example.mebook.domain.LocalRepository
+import com.example.mebook.domain.RemoteRepository
 import com.example.mebook.domain.TestRepository
 import dagger.Binds
 import dagger.Module
@@ -15,5 +21,17 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun provideTestRepository(testRepositoryImpl: TestRepositoryImpl): TestRepository
+
+    @Binds
+    @Singleton
+    abstract fun provideLocalRepository(localRepositoryImpl: LocalRepositoryImpl): LocalRepository
+
+    @Binds
+    @Singleton
+    abstract fun provideRemoteRepository(remoteRepositoryImpl: RemoteRepositoryImpl): RemoteRepository
+
+    @Binds
+    @Singleton
+    abstract fun provideDataStoreRepository(dataStoreRepositoryImpl: DataStoreRepositoryImpl): DataStoreRepository
 
 }
