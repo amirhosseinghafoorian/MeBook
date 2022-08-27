be7b6881eadae8da25d44a71930dc07d92cd1453
AmirHossein <amirhosseinghafoorian>
2022-08-27 21:03:07 +0430

    - clean up

--- app/src/main/java/com/example/mebook/ui/presentation/home/HomeUiState.kt
+++ app/src/main/java/com/example/mebook/ui/presentation/home/HomeUiState.kt
@@ -3,9 +3,9 @@ package com.example.mebook.ui.presentation.home
 import com.example.mebook.model.view.ArticleItemView
 
 data class HomeUiState(
-    val isLoading : Boolean = false,
-    val canShowMoreFeed : Boolean = false,
-    val canShowMoreFeatured : Boolean = false,
+    val isLoading: Boolean = false,
+    val canShowMoreFeed: Boolean = false,
+    val canShowMoreFeatured: Boolean = false,
     val feed: List<ArticleItemView> = listOf(),
     val featured: List<ArticleItemView> = listOf()
 )
